8f303ea3eb8ecd577cf093ed8bb322c4f76fc586
amadawni <61642500+amadawni@users.noreply.github.com>
2023-07-18 23:12:34 +0530

    edited gitignore

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config({ path: "../.env" });;
+require("dotenv").config({ path: ".env" });;
 
 // Create an instance of Express
 const app = express();
